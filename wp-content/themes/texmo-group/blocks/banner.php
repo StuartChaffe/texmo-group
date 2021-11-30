8937996f4392e5a07089881fc86bba679c6156f8
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-11-30 21:21:21 +0000

    Style banner

--- wp-content/themes/texmo-group/blocks/banner.php
+++ wp-content/themes/texmo-group/blocks/banner.php
@@ -3,20 +3,15 @@
 * The template used for displaying a banner.
 */
 $image = get_field('banner_image');
-$imagemobile = get_field('banner_image_mobile');
-$title = get_field('banner_title', false, false);
+$content = get_field('banner_content');
 ?>
 
-<section class="banner">
-	<div class="banner--image" style="background-image: url('<?php echo $image['url']; ?>')">
-		<img loading="lazy" class="hidemobile" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-		<?php if ( $imagemobile ) { ?><img loading="lazy" class="hidedesktop" src="<?php echo $imagemobile['url']; ?>" alt="<?php echo $imagemobile['alt']; ?>" /><?php } ?>
-	</div>
-	<?php if ( $title ) { ?>
-	<div class="container">
-		<div class="banner--title">
-			<h1><?php echo $title ?></h1>
+<section class="banner" style="background-image: url('<?php echo $image['banner_image_large']['url']; ?>')">
+	<?php if ( $content ) { ?>
+		<div class="banner--content">
+			<?php echo $content ?>
 		</div>
-	</div>
 	<?php } ?>
+	<img loading="lazy" class="hidemobile" src="<?php echo $image['banner_image_large']['url']; ?>" alt="<?php echo $image['banner_image_large']['alt']; ?>" />
+	<?php if ( $image['banner_image_mobile'] ) { ?><img loading="lazy" class="hidedesktop" src="<?php echo $image['banner_image_mobile']['url']; ?>" alt="<?php echo $image['banner_image_mobile']['alt']; ?>" /><?php } ?>
 </section>
--- wp-content/themes/texmo-group/page.php
+++ wp-content/themes/texmo-group/page.php
@@ -1,10 +1,7 @@
 <?php get_header(); ?>
 
 <?php while ( have_posts() ) : the_post(); ?>
-	<main class="main content">
-		<h1><?php the_title(); ?></h1>
-		<?php the_content(); ?>
-	</main>
+	<?php the_content(); ?>
 <?php endwhile; ?>
 
 <?php get_footer(); ?>
