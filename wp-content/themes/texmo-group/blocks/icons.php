6111becb661bcb98dcb50792415715aa71703e5b
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-01-06 16:53:49 +0000

    Style icon block

--- wp-content/themes/texmo-group/blocks/icons.php
+++ wp-content/themes/texmo-group/blocks/icons.php
@@ -5,18 +5,20 @@
 $bkg = get_field('bkg-colour');
 ?>
 
-<section class="icons <?php echo $bkg ?>">
-<?php if( have_rows('icons') ) { ?>
-	<?php while( have_rows('icons') ): the_row();
-		$icon = get_sub_field('icon');
-		$content = get_sub_field('content');
-	?>
-	<div class="icons-item">
-		<div class="icons-item--image">
-			<img src="<?php echo $icon['url']; ?>" alt="<?php echo $icon['alt']; ?>" />
+<section class="<?php echo $bkg ?>">
+	<div class="icons">
+	<?php if( have_rows('icons') ) { ?>
+		<?php while( have_rows('icons') ): the_row();
+			$icon = get_sub_field('icon');
+			$content = get_sub_field('content');
+		?>
+		<div class="icons-item">
+			<div class="icons-item--image">
+				<img src="<?php echo $icon['url']; ?>" alt="<?php echo $icon['alt']; ?>" />
+			</div>
+			<p><?php echo $content; ?>
 		</div>
-		<p><?php echo $content; ?>
+		<?php endwhile; ?>
+	<?php } ?>
 	</div>
-	<?php endwhile; ?>
-<?php } ?>
 </section>
