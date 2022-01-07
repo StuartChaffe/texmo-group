196f892b557f637c029ae07055112c8d3800645a
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-01-07 14:50:04 +0000

    Style text image square

--- wp-content/themes/texmo-group/blocks/text-image.php
+++ wp-content/themes/texmo-group/blocks/text-image.php
@@ -5,15 +5,16 @@
 $content = get_field('text-image-content');
 $bkg = get_field('bkg-colour');
 $image = get_field('image');
+$type = get_field('text-image-type');
 ?>
 
-<section class="text-image <?php echo $bkg ?> <?php echo $image['text-image-position'] ?>">
-	<div class="text-image--content">
+<section class="text-image <?php echo $bkg ?> <?php echo $image['text-image-position'] ?><?php if ( $type == 'Square' ) { ?> text-image__square<?php } ?>">
+	<div class="text-image--content<?php if ( $type == 'Square' ) { ?> text-image--content__square<?php } ?>">
 		<div class="text-image--content-inner">
 			<?php echo $content ?>
 		</div>
 	</div>
-	<div class="text-image--image<?php if ( $image['text-image-mobile'] == '1' ) { ?> hidemobile<?php } ?>" style="background-image: url('<?php echo $image['text-image-image']['url']; ?>')">
+	<div class="text-image--image<?php if ( $image['text-image-mobile'] == '1' ) { ?> hidemobile<?php } ?><?php if ( $type == 'Square' ) { ?> text-image--image__square<?php } ?>" style="background-image: url('<?php echo $image['text-image-image']['url']; ?>')">
 		<img loading="lazy" src="<?php echo $image['text-image-image']['url']; ?>" alt="<?php echo $image['text-image-image']['alt']; ?>" />
 	</div>
 </section>
