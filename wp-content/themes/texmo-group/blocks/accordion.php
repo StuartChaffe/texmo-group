e7d578cb4bd263b796be036c011055affc3c30b7
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-01-21 16:32:55 +0000

    Add image to accordion

--- wp-content/themes/texmo-group/blocks/accordion.php
+++ wp-content/themes/texmo-group/blocks/accordion.php
@@ -6,22 +6,30 @@ $bkg = get_field('bkg-colour');
 $title = get_field( 'accordion_title');
 ?>
 <section class="accordion <?php echo $bkg ?>">
-		<?php if ( $title ) { ?><h2><?php echo $title ?></h2><?php } ?>
-		<?php if( have_rows('accordion') ): ?>
-			<ul class="accordion-list">
-			<?php while( have_rows('accordion') ): the_row();
-				$title = get_sub_field('accordion_title', false, false);
-				$content = get_sub_field('accordion_content');
-			?>
-				<li>
-					<button class="accordion--title" aria-expanded="false"><?php echo $title ?> <svg class="icon icon--open"><use xlink:href="#accordion-arrow"></use></svg></button>
-					<div class="accordion--content">
-						<div class="accordion--content-text">
-							<?php echo $content ?>
-						</div>
+	<?php if ( $title ) { ?><h2><?php echo $title ?></h2><?php } ?>
+	<?php if( have_rows('accordion') ): ?>
+		<ul class="accordion-list">
+		<?php while( have_rows('accordion') ): the_row();
+			$title = get_sub_field('accordion_title', false, false);
+			$content = get_sub_field('accordion_content');
+			$image = get_sub_field('accordion_image');
+		?>
+			<li>
+				<button class="accordion--title" aria-expanded="false"><?php echo $title ?> <svg class="icon icon--open"><use xlink:href="#accordion-arrow"></use></svg></button>
+				<div class="accordion--content">
+
+				<?php if ( $image ) { ?>
+					<div class="accordion--content-image">
+						<img loading="lazy" class="" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
 					</div>
-				</li>
-			<?php endwhile; ?>
-			</ul>
-		<?php endif; ?>
+				<?php } ?>
+
+					<div class="accordion--content-text">
+						<?php echo $content ?>
+					</div>
+				</div>
+			</li>
+		<?php endwhile; ?>
+		</ul>
+	<?php endif; ?>
 </section>
