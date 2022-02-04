d7127b50d72fbd955993516ab18b177726f77e07
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-02-04 14:41:18 +0000

    Allow SVG

--- wp-content/themes/texmo-group/lib/images.php
+++ wp-content/themes/texmo-group/lib/images.php
@@ -34,3 +34,9 @@ function add_file_types_to_uploads($file_types){
 	return $file_types;
 }
 add_filter('upload_mimes', 'add_file_types_to_uploads');
+
+function allow_svg_filetype( $mimes ) {
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+  }
+  add_filter( 'upload_mimes', 'allow_svg_filetype' );
