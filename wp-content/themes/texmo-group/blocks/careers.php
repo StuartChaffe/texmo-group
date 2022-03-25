584cd31b597208054ee37dbcb275bb8f7d4fdc2f
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-03-25 13:58:07 +0000

    Attempt API list

--- wp-content/themes/texmo-group/blocks/careers.php
+++ wp-content/themes/texmo-group/blocks/careers.php
@@ -2,10 +2,23 @@
 /**
 * The template used for displaying careers.
 */
-$title = get_field('featured_posts_title');
-$featuredposts = get_field( 'select_posts' );
+
 ?>
 
 <section class="bkg--cream" style="height: 300px; display: flex; align-items: center; justify-content: center;">
 	<h2>Latest Careers</h2>
 </section>
+
+<?php 
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://api-qa.texmo.com/api/recruitment-management/jobs",
+	CURLOPT_HTTPHEADER => array(
+		"accept: text/plain",
+		"TEXMO-KEY: 2c589ffd7309c8679015e0ccc7aea9ee6568ed5f0ae821d5e6522e6cf7d39b23"
+	),
+	));
+	$response = curl_exec($curl);
+	print_r($response);
+	die();
+?>
